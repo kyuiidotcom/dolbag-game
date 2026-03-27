bbaa62e1914e7e185c6ba1e656724c0c8325b710
kyuiidotcom <csakic21c.lab@gmail.com>
2026-03-27 16:27:50 +0900

    app.js 업데이트

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function changeCharacter(type) {
 function equipItem(item) {
   const character = document.getElementById("character");
 
-  if (item === "hat") {
+  if (item === "hat1") {
     character.style.filter = "brightness(1.2)";
   }
 
@@ -33,7 +33,36 @@ function equipItem(item) {
     character.style.filter = "contrast(1.5)";
   }
 
-  if (item === "helmet") {
+  if (item === "helmet1") {
     character.style.transform = "scale(1.1)";
+  }
+}
+
+function changeCharacter(type) {
+  const character = document.getElementById("character");
+
+  if (type === "human1") {
+    character.src = "images/human1.png";
+  }
+
+  if (type === "space1") {
+    character.src = "images/space1.png";
+  }
+}
+
+function equipItem(item) {
+  const character = document.getElementById("character");
+
+  // 간단한 테스트 로직
+  if (item === "hat") {
+    alert("모자 장착");
+  }
+
+  if (item === "glasses") {
+    alert("안경 장착");
+  }
+
+  if (item === "helmet") {
+    alert("헬멧 장착");
   }
 }
