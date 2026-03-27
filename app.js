b2800710cae9cc08d0d1e56862f8c32178191cf0
kyuiidotcom <csakic21c.lab@gmail.com>
2026-03-27 15:18:28 +0900

    app.js 업데이트

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
-function addItem(src) {
-  const canvas = document.getElementById("canvas");
+let currentCharacter = "human";
 
-  const img = document.createElement("img");
-  img.src = "images/" + src;
-  img.className = "item";
+function changeCharacter(type) {
+  currentCharacter = type;
 
-  img.style.left = Math.random() * 300 + "px";
-  img.style.top = Math.random() * 500 + "px";
-  img.style.width = "60px";
+  const img = document.getElementById("character");
 
-  canvas.appendChild(img);
-}
-function setCharacter(src) {
-  const canvas = document.getElementById("canvas");
-  canvas.innerHTML = `<img src="images/${src}" width="200" style="position:absolute; left:100px; top:200px;">`;
+  if (type === "human") {
+    img.src = "images/human1.png";
+  }
+
+  if (type === "space") {
+    img.src = "images/space1.png";
+  }
 }
 
-const characters = [
-  { id: 1, name: "봄_남", type: "human", season: "spring" },
-  { id: 2, name: "봄_여", type: "human", season: "spring" },
-  { id: 3, name: "여름_남", type: "human", season: "summer" },
-  { id: 4, name: "여름_여", type: "human", season: "summer" },
-  { id: 5, name: "가을_남", type: "human", season: "autumn" },
-  { id: 6, name: "가을_여", type: "human", season: "autumn" },
-  { id: 7, name: "겨울_남", type: "human", season: "winter" },
-  { id: 8, name: "겨울_여", type: "human", season: "winter" },
-  { id: 9, name: "우주인_남", type: "space", season: "none" },
-  { id: 10, name: "우주인_여", type: "space", season: "none" }
-];
+function equipItem(item) {
+  const character = document.getElementById("character");
 
-function selectCharacter(id) {
-  const character = characters.find(c => c.id === id);
+  if (item === "hat") {
+    character.style.filter = "brightness(1.2)";
+  }
 
-  document.getElementById("mainImage").src =
-    "images/" + character.name + ".png";
+  if (item === "glasses") {
+    character.style.filter = "contrast(1.5)";
+  }
+
+  if (item === "helmet") {
+    character.style.transform = "scale(1.1)";
+  }
 }
-
