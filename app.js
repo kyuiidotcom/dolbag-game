9aa48aaa39337792faa3ef469bbf57d52bdaca5c
kyuiidotcom <csakic21c.lab@gmail.com>
2026-03-27 10:22:15 +0900

    app.js 업데이트

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+function addItem(src) {
+  const canvas = document.getElementById("canvas");
 
+  const img = document.createElement("img");
+  img.src = "images/" + src;
+  img.className = "item";
+
+  img.style.left = Math.random() * 300 + "px";
+  img.style.top = Math.random() * 500 + "px";
+  img.style.width = "60px";
+
+  canvas.appendChild(img);
+}
