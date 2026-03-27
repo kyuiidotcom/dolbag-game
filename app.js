80d9d6a1777174c45c6ba67b40c0c9f7c2737d80
kyuiidotcom <csakic21c.lab@gmail.com>
2026-03-27 11:14:12 +0900

    app.js 업데이트

--- app.js
+++ app.js
@@ -15,3 +15,16 @@ function setCharacter(src) {
   const canvas = document.getElementById("canvas");
   canvas.innerHTML = `<img src="images/${src}" width="200" style="position:absolute; left:100px; top:200px;">`;
 }
+
+const characters = [
+  { id: 1, name: "봄_남", type: "human", season: "spring" },
+  { id: 2, name: "봄_여", type: "human", season: "spring" },
+  { id: 3, name: "여름_남", type: "human", season: "summer" },
+  { id: 4, name: "여름_여", type: "human", season: "summer" },
+  { id: 5, name: "가을_남", type: "human", season: "autumn" },
+  { id: 6, name: "가을_여", type: "human", season: "autumn" },
+  { id: 7, name: "겨울_남", type: "human", season: "winter" },
+  { id: 8, name: "겨울_여", type: "human", season: "winter" },
+  { id: 9, name: "우주인_남", type: "space", season: "none" },
+  { id: 10, name: "우주인_여", type: "space", season: "none" }
+];
